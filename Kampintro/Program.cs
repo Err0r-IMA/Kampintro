d6e106da0e46093d1b3290c95a0a75fb7b45ebc4
Murat ahmet Kıroğlu <mrt_drncli4161@outlook.com>
2021-02-05 14:16:42 +0300

    Add project files.

---
+++ Kampintro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Kampintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //do not repeat yourself - Kendini  tekrarlama
+            string KategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+
+
+            //if (sistemeGirisYapmisMi == true)     
+            //{
+            //    Console.WriteLine(" Kullanıcı Ayarları butonu");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Giriş Yap Butonu");
+            //}
+            
+            
+            //Console.WriteLine(KategoriEtiketi);
+
+        }
+    }
+}
