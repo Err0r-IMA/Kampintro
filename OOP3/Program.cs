7bb0722779ed86c2f213938f74d9f5a67502dd87
Murat ahmet Kıroğlu <mrt_drncli4161@outlook.com>
2021-02-07 21:23:58 +0300

    7.02.2021

---
+++ OOP3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager(); //interfaceler de o interfaci implemente eden classın referans numarasını tutabiliyor.
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService dataBaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLogerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(konutKrediManager,dataBaseLoggerService);
+
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager,tasitKrediManager};
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+            
+        }
+    }
+}
---
+++ Oop2/MusteriManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+     //iş sınıfları //Business Classes
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            //if (true)
+            //{
+
+            //}
+        }
+
+    }
+}
